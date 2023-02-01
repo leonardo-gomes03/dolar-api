a0654cf6157e13e16d4b4b822807c7b6285b3c6b
leonardo-gomes03 <leogomes.pereira03@gmail.com>
2023-02-01 11:56:55 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const Table = require("cli-table");
+const fs = require('fs');
+const csv = require('fast-csv');
+const j2c = require('json2csv');
+const papaparse = require('papaparse');
+const { table } = require("console");
+const path = require('path');
+
+const API_URL = 'https://economia.awesomeapi.com.br/json/last/USD-BRL,EUR-BRL,BRL-USD,BRL-EUR';
+
+async function getApi() {
+    const response = await fetch(API_URL);
+    const data = await response.json();
+    const arrayData = Object.values(data);
+
+    arrayData.map((post) => {
+        const table = new Table({
+            head: ['De', 'Para', 'Valor do Cambio'],
+            colWidths: [10, 10, 25]
+        });
+
+        const [headers] = arrayData.map((item) => Object.keys(item));
+        const headersText = `${headers.join(",")}\n`;
+        const pathFile = path.join(__dirname, "cotaçãoatual.csv");
+
+        fs.writeFileSync(pathFile, headersText);
+        const cotacao = arrayData.map((item) =>
+          Object.values(item).join(",")
+        );
+        console.log("cotacao", cotacao);
+        fs.writeFileSync(pathFile, cotacao.join("\n"), { flag: "a" });
+
+        table.push([post.codein, post.code, post.bid]);
+
+        const finalTable = table.toString();
+
+        console.log(finalTable);
+    })
+}
+
+getApi();
+
